91b7f026866cd6de8fe7023c8d71ce4a3e4bac93
Jayasree <jayasree.aparna@lntinfotech.com>
2023-04-13 14:42:58 +0530

    initial commit to Repository

---
+++ src/main/java/com/lti/banking/FundsTransferService/FundsTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lti.banking.FundsTransferService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundsTransferServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FundsTransferServiceApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/lti/banking/FundsTransferService/FundsTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.banking.FundsTransferService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FundsTransferServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
